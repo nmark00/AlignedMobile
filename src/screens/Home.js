93edd058ec248c7a95d0ba06e5d84a8385c1cd30
ME <56805453+nmark00@users.noreply.github.com>
2021-12-28 01:28:11 -0800

    Changed Home to Tab nav

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, FlatList,Alert } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import AddUserScreen from './AddUserScreen';
 import UserScreen from './UserScreen';
 import UserDetailScreen from './UserDetailScreen';
 import Logout from './Logout';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function Home() {
 	return (
-      <Stack.Navigator
+      <Tab.Navigator
       screenOptions={{
           headerStyle: {
             backgroundColor: '#621FF7',
@@ -23,26 +22,26 @@ export default function Home() {
           },
         }}
       >
-      <Stack.Screen 
+      <Tab.Screen 
         name="Logout" 
         component={Logout} 
         options={{ title: 'Logout' }}
       />
-      <Stack.Screen 
+      <Tab.Screen 
         name="AddUserScreen" 
         component={AddUserScreen} 
         options={{ title: 'Add User' }}
       />
-      <Stack.Screen 
+      <Tab.Screen 
         name="UserScreen" 
         component={UserScreen} 
         options={{ title: 'Users List' }}
       />
-      <Stack.Screen 
+      <Tab.Screen 
        name="UserDetailScreen" 
        component={UserDetailScreen} 
        options={{ title: 'User Detail' }}
       />
-    </Stack.Navigator>
+    </Tab.Navigator>
     );
 }
